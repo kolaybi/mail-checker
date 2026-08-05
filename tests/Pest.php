40e250a9e560a996dbed7111688692804607e935
Ugur Ozkan <ugur.ozkan@kolaybi.com>
2026-08-05 11:28:13 +0300

    Fix Pest bindings for root-level tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,5 +4,5 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use KolayBi\Validation\Mail\Tests\SuppressionTestCase;
 use KolayBi\Validation\Mail\Tests\TestCase;
 
-uses(TestCase::class)->in(__DIR__ . '/Console', __DIR__ . '/Providers');
+uses(TestCase::class)->in(__DIR__ . '/*.php', __DIR__ . '/Console', __DIR__ . '/Providers');
 uses(SuppressionTestCase::class, RefreshDatabase::class)->in(__DIR__ . '/Suppression');
